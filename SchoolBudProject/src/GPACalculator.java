04439d6269dafd1fca6ea62909093a757434bf0d
shahdk <shahdk@rose-hulman.edu>
2013-03-16 18:53:51 -0400

    Create and pass the first test for Total Grade Points

--- SchoolBudProject/src/GPACalculator.java
+++ SchoolBudProject/src/GPACalculator.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+
 
 public class GPACalculator {
+
+	public static double TotalGrapdePoints(ArrayList<double[]> list) {
+		
+		double sum = 0;
+		
+		for(double[] val: list){
+			double product = val[0] * val[1];
+			sum+=product;
+		}
+		
+		return sum;
+	}
 
 }
--- SchoolBudProject/src/GPACalculatorTest.java
+++ SchoolBudProject/src/GPACalculatorTest.java
@@ -1,13 +1,31 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 
 public class GPACalculatorTest {
 
+	private static final double DELTA = 1e-15;
+	
 	@Test
 	public void testJUnit() {
 		assertTrue("JUnit works!", true);
+	}
+	
+	@Test
+	public void testTotalGradePointsOne() {
+		double[] val = {0, 4};
+		assertEquals(0, GPACalculator.TotalGrapdePoints(list(val)), DELTA);
+	}
+	
+	private ArrayList<double[]> list(double[]... vals){
+		ArrayList<double[]> ret = new ArrayList<double[]>();
+		for(double[] i: vals){
+			ret.add(i);
+		}
+		return ret;
 	}
 
 }
