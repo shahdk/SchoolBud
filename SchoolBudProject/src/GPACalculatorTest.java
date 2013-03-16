aa10dbdf0093cfdf703f8f6d0f0397f1be0554c0
shahdk <shahdk@rose-hulman.edu>
2013-03-16 18:37:34 -0400

    Create eclipse workspaceCreate test to check if JUnit works

--- SchoolBudProject/src/GPACalculatorTest.java
+++ SchoolBudProject/src/GPACalculatorTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 public class GPACalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testJUnit() {
+		assertTrue("JUnit works!", true);
 	}
 
 }
