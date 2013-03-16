e398bde9f5f1b2530dee6c764101e363c4129c1f
shahdk <shahdk@rose-hulman.edu>
2013-03-16 19:03:11 -0400

    Create and pass the Exception test 1 for Total Grade Points

--- SchoolBudProject/src/GPACalculator.java
+++ SchoolBudProject/src/GPACalculator.java
@@ -8,6 +8,9 @@ public class GPACalculator {
 		double sum = 0;
 		
 		for(double[] val: list){
+			if(val[0] < 0 || val[1] < 0){
+				throw new IllegalArgumentException();
+			}
 			double product = val[0] * val[1];
 			sum+=product;
 		}
--- SchoolBudProject/src/GPACalculatorTest.java
+++ SchoolBudProject/src/GPACalculatorTest.java
@@ -35,6 +35,15 @@ public class GPACalculatorTest {
 		assertEquals(21.0, GPACalculator.TotalGrapdePoints(list(val1, val2, val3, val4)), DELTA);
 	}
 	
+	@Test(expected=IllegalArgumentException.class)
+	public void testTotalGradePointsExceptionOne() {
+		double[] val1 = {3, 4};
+		double[] val2 = {1, 3};
+		double[] val3 = {-3, 2};
+		double[] val4 = {4, 0};
+		GPACalculator.TotalGrapdePoints(list(val1, val2, val3, val4));
+	}
+	
 	private ArrayList<double[]> list(double[]... vals){
 		ArrayList<double[]> ret = new ArrayList<double[]>();
 		for(double[] i: vals){
