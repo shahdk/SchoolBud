af2f1a91ccfd6c49b733defebb84aff10b3e5844
shahdk <shahdk@rose-hulman.edu>
2013-03-16 18:58:23 -0400

    Create and pass the Third test for Total Grade Points

--- SchoolBudProject/src/GPACalculatorTest.java
+++ SchoolBudProject/src/GPACalculatorTest.java
@@ -26,6 +26,15 @@ public class GPACalculatorTest {
 		assertEquals(4.0, GPACalculator.TotalGrapdePoints(list(val)), DELTA);
 	}
 	
+	@Test
+	public void testTotalGradePointsThree() {
+		double[] val1 = {3, 4};
+		double[] val2 = {1, 3};
+		double[] val3 = {3, 2};
+		double[] val4 = {4, 0};
+		assertEquals(21.0, GPACalculator.TotalGrapdePoints(list(val1, val2, val3, val4)), DELTA);
+	}
+	
 	private ArrayList<double[]> list(double[]... vals){
 		ArrayList<double[]> ret = new ArrayList<double[]>();
 		for(double[] i: vals){
