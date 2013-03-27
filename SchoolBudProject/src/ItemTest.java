e160f7bbba22355b69e7d34a96ed858a0fedaeda
shahdk <shahdk@rose-hulman.edu>
2013-03-27 00:26:42 -0400

    Add test cases for getter methods for item

--- SchoolBudProject/src/ItemTest.java
+++ SchoolBudProject/src/ItemTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 
@@ -26,6 +28,38 @@ public class ItemTest {
 	public void testItemInitializeWithTwoParameters(){
 		Item item = new Item("HW1", "0.10");
 		assertNotNull(item);
+	}
+	
+	@Test
+	public void testGetEarnedPoints(){
+		Item item = new Item("HW1", "8.5", "10", "0.10");
+		assertEquals("8.5", item.getEarnedPoints());
+	}
+	
+	@Test
+	public void testGetTotalPoints(){
+		Item item = new Item("HW1", "8.5", "10", "0.10");
+		assertEquals("8.5", item.getTotalPoints());
+	}
+	
+	@Test
+	public void testGetWeight(){
+		Item item = new Item("HW1", "8.5", "10", "0.10");
+		assertEquals("8.5", item.getWeight());
+	}
+	
+	@Test
+	public void testGetName(){
+		Item item = new Item("HW1", "8.5", "10", "0.10");
+		assertEquals("8.5", item.getName());
+	}
+	
+	private ArrayList<String> list(String... vals){
+		ArrayList<String> ret = new ArrayList<String>();
+		for(String i: vals){
+			ret.add(i);
+		}
+		return ret;
 	}
 
 }
