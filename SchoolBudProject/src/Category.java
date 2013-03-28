556f28a71d571374f5c04c3b1f138c7a3c11d85c
shahdk <shahdk@rose-hulman.edu>
2013-03-28 12:26:54 -0400

    Add code for the caclulating the number of items in a Category

--- SchoolBudProject/src/Category.java
+++ SchoolBudProject/src/Category.java
@@ -6,17 +6,20 @@ public class Category {
 	private String catName;
 	private String weight;
 	private ArrayList<Item> items;
+	private int numOfItems;
 	
 	public Category(String name, String weight) {
 		this.catName = name;
 		this.weight = weight;
 		this.items = new ArrayList<Item>();
+		this.numOfItems = this.items.size();
 	}
 
 	public Category(String name, ArrayList<Item> items, String weight) {
 		this.catName = name;
 		this.weight = weight;
 		this.items = items;
+		this.numOfItems = this.items.size();
 	}
 
 	public ArrayList<Item> getItemList() {
@@ -33,6 +36,7 @@ public class Category {
 
 	public void setItemList(ArrayList<Item> items) {
 		this.items = items;
+		this.numOfItems = this.items.size();
 	}
 
 	public void setName(String name) {
@@ -41,6 +45,10 @@ public class Category {
 
 	public void setWeight(String weight) {
 		this.weight = weight;		
+	}
+
+	public int getNumOfItems() {
+		return this.numOfItems;
 	}
 
 }
