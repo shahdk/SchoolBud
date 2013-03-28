5d353142bfb5e733acb6ff2e7a92b18d09969d29
shahdk <shahdk@rose-hulman.edu>
2013-03-27 21:19:32 -0400

    Add code for initializing category object

--- SchoolBudProject/src/Category.java
+++ SchoolBudProject/src/Category.java
@@ -1,4 +1,12 @@
 
 public class Category {
 
+	private String catName;
+	private String weight;
+	
+	public Category(String name, String weight) {
+		this.catName = name;
+		this.weight = weight;
+	}
+
 }
