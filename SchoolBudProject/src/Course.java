a6622843d9f0909bb32cb9227456fb8bb15c6d11
shahdk <shahdk@rose-hulman.edu>
2013-03-31 19:55:34 -0400

    Add code for initializing Course with name

--- SchoolBudProject/src/Course.java
+++ SchoolBudProject/src/Course.java
@@ -1,37 +1,12 @@
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//
-//public class Course {
-//
-//	private Item courseItem;
-//
-//	public Course(String itemName, double weight) {
-//		this.courseItem = new Item(itemName, weight);
-//	}
-//
-//	public Course(String item, ArrayList<String> subItem, double earnedPoints,
-//			double totalPoints) {
-//		this.courseItem = new Item(item, subItem, earnedPoints, totalPoints);
-//	}
-//
-//	private class Item {
-//
-//		private String itemName;
-//		private ArrayList<String> subItem;
-//		private double weight, earnedPoints, totalPoints;
-//
-//		public Item(String itemName, double weight) {
-//			this.itemName = itemName;
-//			this.weight = weight;
-//		}
-//
-//		public Item(String item, ArrayList<String> subItem,
-//				double earnedPoints, double totalPoints) {
-//			this.itemName = item;
-//			this.subItem = subItem;
-//			this.earnedPoints = earnedPoints;
-//			this.totalPoints = totalPoints;
-//		}
-//
-//	}
-//}
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Course {
+
+	private String courseName;
+	
+	public Course(String name){
+		this.courseName = name;
+	}
+	
+}
