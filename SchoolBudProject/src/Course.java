4a0d7164c2474ab4900e8ca5e26f75b003d8675a
shahdk <shahdk@rose-hulman.edu>
2013-03-31 22:09:56 -0400

    Add code for initializing Course with name and credit hours

--- SchoolBudProject/src/Course.java
+++ SchoolBudProject/src/Course.java
@@ -2,9 +2,15 @@
 public class Course {
 
 	private String courseName;
+	private double creditHours;
 	
 	public Course(String name){
 		this.courseName = name;
+	}
+
+	public Course(String name, double creditHours) {
+		this.courseName = name;
+		this.creditHours = creditHours;
 	}
 	
 }
