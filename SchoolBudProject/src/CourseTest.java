5370af1c6aa0453c2429929dbe83beb564d757e3
shahdk <shahdk@rose-hulman.edu>
2013-03-31 22:48:20 -0400

    Add test code for getting name and credit hours

--- SchoolBudProject/src/CourseTest.java
+++ SchoolBudProject/src/CourseTest.java
@@ -25,4 +25,28 @@ public class CourseTest {
 		assertNotNull(new Course("CSSE376", 4.0));
 	}
 
+	//test case for get methods
+	@Test
+	public void testGetNameOne(){
+		Course course = new Course("CSSE376");
+		assertEquals("CSSE376", course.getCourseName());
+	}
+	
+	@Test
+	public void testGetNameTwo(){
+		Course course = new Course("CSSE376", 4.0);
+		assertEquals("CSSE376", course.getCourseName());
+	}
+	
+	@Test
+	public void testGetCreditHoursOne(){
+		Course course = new Course("CSSE376", 4.0);
+		assertEquals(4.0, course.getCreditHours());
+	}
+	
+	@Test
+	public void testGetCreditHoursTwo(){
+		Course course = new Course("CSSE376");
+		assertEquals(0.0, course.getCreditHours());
+	}
 }
