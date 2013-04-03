dace247ec488c21eac9cd99728c9242e355d70a5
shahdk <shahdk@rose-hulman.edu>
2013-04-02 21:50:48 -0400

    Add test code for getting the lower limit, upper limit and gpa for a letter grade

--- SchoolBudProject/src/RubricTest.java
+++ SchoolBudProject/src/RubricTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 public class RubricTest {
 
+	private static final double DELTA = 1e-15;
+	
 	@Test
 	public void testJUnit() {
 		assertTrue("JUnit works!", true);
@@ -13,6 +15,28 @@ public class RubricTest {
 	@Test
 	public void testRubricInitialization(){
 		assertNotNull(new Rubric());
+	}
+	
+	//test cases for getting and setting the letter grade along with max and min limits
+	@Test
+	public void testGetLowerLimit(){
+		Rubric rubric = new Rubric();
+		rubric.addGrade("A", 90, 100, 4.0);
+		assertEquals(90.0, rubric.getLowerLimit("A"), DELTA);
+	}
+	
+	@Test
+	public void testGetUpperLimit(){
+		Rubric rubric = new Rubric();
+		rubric.addGrade("A", 90, 100, 4.0);
+		assertEquals(100.0, rubric.getUpperLimit("A"), DELTA);
+	}
+	
+	@Test
+	public void testGetGPA(){
+		Rubric rubric = new Rubric();
+		rubric.addGrade("A", 90, 100, 4.0);
+		assertEquals(4.0, rubric.getGPA("A"), DELTA);
 	}
 
 }
