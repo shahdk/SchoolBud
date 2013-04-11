313a1add196f512c60b64d9e71642a9888f98104
John <mccormjt@rose-hulman.edu>
2013-04-11 08:42:02 -0400

    added null test

--- SchoolBudProject/src/Scheduler.java
+++ SchoolBudProject/src/Scheduler.java
@@ -7,62 +7,67 @@ import java.util.ArrayList;
 
 public class Scheduler {
 	
-	private ArrayList<Class> classes;
-	private ArrayList<Class[]> schedules;
-	private int classHours;
+	private ArrayList<SchedulerCourse> classes;
+	private ArrayList<ArrayList<SchedulerCourse>> schedules;
+	private int numClassHours;
 
-	public Scheduler(int classHours, ArrayList<Class> classes) {
+	public Scheduler(int classHours, ArrayList<SchedulerCourse> classes) {
 		this.classes = classes;
-		this.classHours = classHours + 1;
-		this.schedules = new ArrayList<Class[]>();
+		this.numClassHours = classHours;
+		this.schedules = new ArrayList<ArrayList<SchedulerCourse>>();
+		
+		//initialize 7 days of the week with an arrayList
+		for (int i = 0; i < 6; i++) {
+			this.schedules.add(new ArrayList<SchedulerCourse>());
+		}
 	}
 	
 	
-	public ArrayList<Class[]> permutateSchedules(){
+	public ArrayList<ArrayList<SchedulerCourse>> permutateSchedules(){
 		
 		this.schedules.clear();
 		
-		if (this.classes == null){
-			return this.schedules;
-		}
-		
-		
-		ArrayList<Integer> taken = new ArrayList<Integer>();
-		@SuppressWarnings("unchecked")
-		ArrayList<Class> tempClasses = (ArrayList<Class>) this.classes.clone();
-		if (this.classes == null){
-			return this.schedules;
-		}
-		
-		for (Class theClass : this.classes) {
-			tempClasses.remove(0);
-			
-			for (Integer hour : theClass.getHours()) {
-				Class[] theHours = new Class[this.classHours];
-				taken.clear();
-				taken.add(hour);
-				theHours[hour] = theClass;
-				
-				
-				for (Class theClassInner : this.classes) {
-					if (theClassInner.equals(theClass)){
-						continue;
-					}
-					for (Integer hourInner : theClassInner.getHours()) {
-						if (! taken.contains(hourInner)){
-							theHours[hourInner] = theClassInner;
-							taken.add(hourInner);
-;						}
-					}
-					
-				}
-				
-				if (this.isFull(theHours)) {
-					this.schedules.add(theHours);
-				}
-			}
-			
-		}
+//		if (this.classes == null){
+//			return this.schedules;
+//		}
+//		
+//		
+//		ArrayList<Integer> taken = new ArrayList<Integer>();
+//		@SuppressWarnings("unchecked")
+//		ArrayList<Class> tempClasses = (ArrayList<Class>) this.classes.clone();
+//		if (this.classes == null){
+//			return this.schedules;
+//		}
+//		
+//		for (Class theClass : this.classes) {
+//			tempClasses.remove(0);
+//			
+//			for (Integer hour : theClass.getHours()) {
+//				Class[] theHours = new Class[this.classHours];
+//				taken.clear();
+//				taken.add(hour);
+//				theHours[hour] = theClass;
+//				
+//				
+//				for (Class theClassInner : this.classes) {
+//					if (theClassInner.equals(theClass)){
+//						continue;
+//					}
+//					for (Integer hourInner : theClassInner.getHours()) {
+//						if (! taken.contains(hourInner)){
+//							theHours[hourInner] = theClassInner;
+//							taken.add(hourInner);
+//;						}
+//					}
+//					
+//				}
+//				
+//				if (this.isFull(theHours)) {
+//					this.schedules.add(theHours);
+//				}
+//			}
+//			
+//		}
 			
 		return this.schedules;
 	}
