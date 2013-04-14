86af7568c44ea2028f1da2e6e34ceaf57034b450
John <mccormjt@rose-hulman.edu>
2013-04-14 15:38:41 -0400

    finished revamping SchedulercourseTester

--- SchoolBudProject/src/SchedulerCourseTester.java
+++ SchoolBudProject/src/SchedulerCourseTester.java
@@ -27,49 +27,46 @@ public class SchedulerCourseTester {
 
 	}
 
-//	@Test
-//	public void testSetGetScheduleHours() {
-// 
-//		ArrayList<ClassDay> days = WeekSchedule.create7DayArrayList();
-//		ArrayList<Integer> hours = new ArrayList<Integer>();
-//		hours.add(1);
-//		hours.add(5);
-//		ClassDay day = new ClassDay(hours);
-//		days.set(0, day);
-//		WeekSchedule week = new WeekSchedule(days);
-//		ArrayList<WeekSchedule> sections = new ArrayList<WeekSchedule>();
-//		sections.add(week);
-//		ClassSection sectionHolder = new ClassSection(sections);
-//
-//		SchedulerCourse class1 = new SchedulerCourse("Math", "Mr. Man",
-//				sectionHolder);
-//		assertEquals(class1.getSections(), sectionHolder);
-//
-//		SchedulerCourse class2 = new SchedulerCourse("Physics", "Mr. Much",
-//				null);
-//		class2.setSections(class1.getSections());
-//		assertEquals(class2.getSections(), sectionHolder);
-//
-//	}
-//
-//	@Test
-//	public void testTeacher() {
-//
-//		ArrayList<ClassDay> days = WeekSchedule.create7DayArrayList();
-//		ArrayList<Integer> hours = new ArrayList<Integer>();
-//		hours.add(1);
-//		hours.add(5);
-//		ClassDay day = new ClassDay(hours);
-//		days.set(0, day);
-//		WeekSchedule week = new WeekSchedule(days);
-//		ArrayList<WeekSchedule> sections = new ArrayList<WeekSchedule>();
-//		sections.add(week);
-//		ClassSection sectionHolder = new ClassSection(sections);
-//
-//		SchedulerCourse class1 = new SchedulerCourse("Math", "Mr. Man", sectionHolder);
-//		assertEquals(class1.getTeacher(), "Mr. Man");
-//		class1.setTeacher("Mrs. Lady");
-//		assertEquals(class1.getTeacher(), "Mrs. Lady");
-//
-//	}
+	@Test
+	public void testSetGetScheduleHours() {
+ 
+		ArrayList<ClassDay> days = ClassSection.create7DayArrayList();
+		ArrayList<Integer> hours = new ArrayList<Integer>();
+		hours.add(1);
+		hours.add(5);
+		ClassDay day = new ClassDay(hours);
+		days.set(0, day);
+		ClassSection section = new ClassSection("Mr. John", days);
+		ArrayList<ClassSection> sections = new ArrayList<ClassSection>();
+		sections.add(section);
+
+		SchedulerCourse class1 = new SchedulerCourse("Math", sections, false);
+		assertEquals(class1.getSections(), sections);
+
+		SchedulerCourse class2 = new SchedulerCourse("Physics", null, false);
+		class2.setSections(class1.getSections());
+		assertEquals(class2.getSections(), sections);
+
+	}
+
+	@Test
+	public void testTeacher() {
+
+		ArrayList<ClassDay> days = ClassSection.create7DayArrayList();
+		ArrayList<Integer> hours = new ArrayList<Integer>();
+		hours.add(1);
+		hours.add(5);
+		ClassDay day = new ClassDay(hours);
+		days.set(0, day);
+		ClassSection section = new ClassSection("Mr. Mackey", days);
+		ArrayList<ClassSection> sections = new ArrayList<ClassSection>();
+		sections.add(section);
+
+		SchedulerCourse class1 = new SchedulerCourse("Math", sections, false);
+		
+		assertEquals(class1.getSections().get(0).getTeacher(), "Mr. Mackey");
+		class1.getSections().get(0).setTeacher("Mrs. Lady");
+		assertEquals(class1.getSections().get(0).getTeacher(), "Mrs. Lady");
+
+	}
 }
