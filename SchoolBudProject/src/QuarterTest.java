2e7c810985fb5633b1b72255c6660e5ec98c6489
shahdk <shahdk@rose-hulman.edu>
2013-04-16 17:53:49 -0400

    Add test cases for getting the total credit hours in quarter

--- SchoolBudProject/src/QuarterTest.java
+++ SchoolBudProject/src/QuarterTest.java
@@ -3,6 +3,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class QuarterTest {
+	
+	private static final double DELTA = 1e-15;
 
 	@Test
 	public void testJUnit() {
@@ -82,5 +84,44 @@ public class QuarterTest {
 		qt.addCourse(course1);
 		assertTrue(qt.removeCourse(course));
 		assertEquals("CSSE290", qt.getCourseList().get(0).getCourseName());
+	}
+	
+	//test cases for getting the total credit hours taken in a quarter
+	@Test
+	public void testGetTotalCreditHoursOne(){
+		Quarter qt = new Quarter("Spring2013");
+		Course course = new Course("CSSE376", 4);
+		Course course1 = new Course("CSSE290", 4);
+		qt.addCourse(course);
+		qt.addCourse(course1);
+		assertEquals(8.0, qt.getTotalCreditHours(), DELTA);
+	}
+	
+	@Test
+	public void testGetTotalCreditHoursTwo(){
+		Quarter qt = new Quarter("Spring2013");
+		assertEquals(0.0, qt.getTotalCreditHours(), DELTA);
+	}
+	
+	@Test
+	public void testGetTotalCreditHoursThree(){
+		Quarter qt = new Quarter("Spring2013");
+		Course course = new Course("CSSE376", 4);
+		Course course1 = new Course("CSSE290", 4);
+		qt.addCourse(course);
+		qt.addCourse(course1);
+		qt.removeCourse(course1);
+		assertEquals(4.0, qt.getTotalCreditHours(), DELTA);
+	}
+	
+	@Test
+	public void testGetTotalCreditHoursFour(){
+		Quarter qt = new Quarter("Spring2013");
+		Course course = new Course("CSSE376", 3);
+		Course course1 = new Course("CSSE290", 4);
+		qt.addCourse(course);
+		qt.addCourse(course1);
+		qt.removeCourse(course1);
+		assertEquals(3.0, qt.getTotalCreditHours(), DELTA);
 	}
 }
