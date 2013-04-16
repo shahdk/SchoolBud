fa89d64957e943b5cce9e8b0690982c2d925bbdd
shahdk <shahdk@rose-hulman.edu>
2013-04-15 23:43:40 -0400

    Add test case for add course and getting a course

--- SchoolBudProject/src/Quarter.java
+++ SchoolBudProject/src/Quarter.java
@@ -10,4 +10,16 @@ public class Quarter {
 		this.quaterName = name;
 	}
 
+	public String getName() {
+		return this.quaterName;
+	}
+
+	public void setName(String name) {
+		if(name.length()==0){
+			throw new IllegalArgumentException();
+		}
+		this.quaterName = name;
+	}
+
+
 }
--- SchoolBudProject/src/QuarterTest.java
+++ SchoolBudProject/src/QuarterTest.java
@@ -2,7 +2,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class QuarterTest {
 
 	@Test
@@ -23,18 +22,27 @@ public class QuarterTest {
 	@Test
 	public void testGetNameOne(){
 		Quarter qt = new Quarter("Spring2013");
-		assertEquals("Spring2013", qt.getName);
+		assertEquals("Spring2013", qt.getName());
 	}
 	
 	@Test
 	public void testSetNameOne(){
 		Quarter qt = new Quarter("Spring2013");
 		qt.setName("Spring2012-13");
-		assertEquals("Spring2012-13", qt.getName);
+		assertEquals("Spring2012-13", qt.getName());
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testSetNameTwo(){
 		Quarter qt = new Quarter("Spring2013");
 		qt.setName("");	}
+	
+	//test cases for getting and adding courses
+	@Test
+	public void testAddCourseOne(){
+		Quarter qt = new Quarter("Spring2013");
+		Course course = new Course("CSSE376");
+		qt.addCourse(course);
+		assertEquals("CSSE376", qt.getCourseList().get(0).getName());
+	}
 }
