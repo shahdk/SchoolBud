74757bbe0483a7c80fe4e8722c329c15b3d99a51
padillbt <padillbt@rose-hulman.edu>
2013-04-25 00:58:58 -0400

    completed dialog boxes with constructors and added tables

---
+++ SchoolBudProject/src/Main.java
@@ -0,0 +1,20 @@
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author padillbt-1.
+ *         Created Apr 23, 2013.
+ */
+public class Main {
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SchoolBudGUI gui = new SchoolBudGUI();
+		gui.setVisible(true);
+	}
+
+}
