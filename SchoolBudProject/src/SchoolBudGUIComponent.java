760cb5f1266c4dfd8a300c62070695d0af820646
shahdk <shahdk@rose-hulman.edu>
2013-05-03 12:27:07 -0400

    Edit and remove items in GUI

--- SchoolBudProject/src/SchoolBudGUIComponent.java
+++ SchoolBudProject/src/SchoolBudGUIComponent.java
@@ -2,6 +2,8 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
@@ -11,7 +13,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+import javax.swing.JTable;
 
 /**
  * TODO Put here a description of what this class does.
@@ -33,9 +35,10 @@ public class SchoolBudGUIComponent extends JPanel implements ActionListener {
 
 		String[] quarterStrings = { "----" };
 
-		String[] names = {"Item Name", "Earned Points", "Total Points", "Update Date", "Category", "Remove"};
+		String[] names = { "Item Name", "Earned Points", "Total Points",
+				"Update Date", "Category", "Remove" };
 		this.table = new SchoolBudGUITable(names);
-		
+
 		this.quarterList = new JComboBox(quarterStrings);
 		this.quarterList.setSelectedIndex(0);
 		this.quarterList.addActionListener(new ActionListener() {
@@ -50,7 +53,7 @@ public class SchoolBudGUIComponent extends JPanel implements ActionListener {
 
 		this.classList = new JComboBox();
 		this.classList.setPrototypeDisplayValue("XXXXXXXXXX");
-		this.classList.addActionListener(new ActionListener(){
+		this.classList.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent event) {
 				JComboBox box = (JComboBox) event.getSource();
@@ -59,13 +62,12 @@ public class SchoolBudGUIComponent extends JPanel implements ActionListener {
 			}
 		});
 
-		
 		this.comboPanel = new JPanel();
 		this.comboPanel.setLayout(new BorderLayout());
-		
+
 		this.comboPanel.add(this.quarterList, BorderLayout.NORTH);
 		this.comboPanel.add(this.classList, BorderLayout.CENTER);
-		
+
 		add(this.comboPanel, BorderLayout.PAGE_START);
 		add(this.table.getJScrollPane(), BorderLayout.CENTER);
 		this.classList.setSize(this.quarterList.getSize());
@@ -104,36 +106,42 @@ public class SchoolBudGUIComponent extends JPanel implements ActionListener {
 			}
 		}
 	}
-	
-	public void updateTable(String name){
+
+	public void updateTable(String name) {
 		this.table.reset();
 		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
-		for(Quarter current: this.quarters){
-			for(Course course: current.getCourseList()){
-				if(course.getCourseName().equals(name)){
-					for(Category cat: course.getCategories()){
+		for (Quarter current : this.quarters) {
+			for (Course course : current.getCourseList()) {
+				if (course.getCourseName().equals(name)) {
+					for (Category cat : course.getCategories()) {
 						int numItems = cat.getItemList().size();
-						
+
 						Object[][] newItems = new Object[numItems][this.NUM_COLS];
-						
+
 						for (int i = 0; i < numItems; i++) {
-							
+
 							Object[] info = new Object[this.NUM_COLS];
 							info[0] = (cat.getItemList().get(i).getName());
-							info[1] = cat.getItemList().get(i).getEarnedPoints();
+							info[1] = cat.getItemList().get(i)
+									.getEarnedPoints();
 							info[2] = cat.getItemList().get(i).getTotalPoints();
-							info[3] = sdf.format(cat.getItemList().get(i).getUpdateDate());
+							info[3] = sdf.format(cat.getItemList().get(i)
+									.getUpdateDate());
 							info[4] = cat.getName();
 							info[5] = false;
 							newItems[i] = info;
 						}
-						
+
 						this.table.addItems(newItems, numItems);
 					}
+					this.table.setQuarters(quarters, getSelectedQuarter(), getSelectedCourse());
 					return;
 				}
 			}
 		}
+
+		
+
 	}
 
 	@Override
@@ -183,8 +191,8 @@ public class SchoolBudGUIComponent extends JPanel implements ActionListener {
 			}
 		}
 	}
-	
-	public void editQuarter(String name){
+
+	public void editQuarter(String name) {
 		for (Quarter current : this.quarters) {
 			if (current.getName().equals(getSelectedQuarter())) {
 				current.setName(name);
@@ -193,10 +201,10 @@ public class SchoolBudGUIComponent extends JPanel implements ActionListener {
 			break;
 		}
 	}
-	
-	public void updateHeadings(ArrayList<String> headings){
+
+	public void updateHeadings(ArrayList<String> headings) {
 		String[] newHeadings = new String[headings.size()];
-		for(int i = 0; i<headings.size(); i++){
+		for (int i = 0; i < headings.size(); i++) {
 			newHeadings[i] = headings.get(i);
 		}
 		this.table.changeHeaderValues(newHeadings);
--- SchoolBudProject/src/SchoolBudGUITable.java
+++ SchoolBudProject/src/SchoolBudGUITable.java
@@ -2,17 +2,27 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
+import javax.swing.AbstractAction;
 import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.KeyStroke;
 import javax.swing.UIManager;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.*;
 
 /**
  * TODO Put here a description of what this class does.
@@ -24,6 +34,9 @@ public class SchoolBudGUITable {
 	private JScrollPane tableSP;
 	private DefaultTableModel tableModel;
 	private String[] columnNames;
+	private ArrayList<Quarter> quarters;
+	private String selectedQuarter;
+	private String selectedCourse;
 
 	public SchoolBudGUITable(String[] names) {
 		this.columnNames = names;
@@ -32,30 +45,150 @@ public class SchoolBudGUITable {
 
 		this.tableModel = new DefaultTableModel(data, this.columnNames);
 
-		this.table = new JTable(this.tableModel){
+		this.table = new JTable(this.tableModel) {
 			private static final long serialVersionUID = 1L;
+
 			@Override
-            public Class getColumnClass(int column) {
-                switch (column) {
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                        return String.class;
-                    default:
-                        return Boolean.class;
-                }
-            }
+			public Class getColumnClass(int column) {
+				switch (column) {
+				case 0:
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+					return String.class;
+				default:
+					return Boolean.class;
+				}
+			}
 		};
-		table.setPreferredScrollableViewportSize(new Dimension(500, 300));
-		table.setFillsViewportHeight(true);
+
+		this.table.setPreferredScrollableViewportSize(new Dimension(500, 300));
+		this.table.setFillsViewportHeight(true);
 		this.tableSP = new JScrollPane(table);
-		tableSP.setPreferredSize(new Dimension(500, 400));
+		this.tableSP.setPreferredSize(new Dimension(500, 400));
+
+		this.table.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+
+				JTable target = (JTable) e.getSource();
+				int row = target.getSelectedRow();
+				int column = target.getSelectedColumn();
+				if (column == 5) {
+					removeItem(row, column);
+				}
+			}
+
+		});
+		
+		TableCellListener tcl = new TableCellListener();
+		this.table.addPropertyChangeListener(tcl);
+	}
+
+	private class TableCellListener implements PropertyChangeListener{
+
+		@Override
+		public void propertyChange(PropertyChangeEvent e) {
+			
+			if ("tableCellEditor".equals(e.getPropertyName()))
+			{
+				if (!table.isEditing()){
+					int row = table.getSelectedRow();
+					int col = table.getSelectedColumn();
+					for (Quarter current : quarters) {
+						if (current.getName().equals(selectedQuarter)) {
+							ArrayList<Course> currentCourses = current.getCourseList();
+							for (Course c : currentCourses) {
+								if (c.getCourseName().equals(selectedCourse)) {
+									String catName = (String) table.getValueAt(row, 4);
+									String itemName = (String) table
+											.getValueAt(row, 0);
+
+									for (int i = 0; i < c.getCategories().size(); i++) {
+										if (c.getCategories().get(i).getName()
+												.equals(catName)) {
+											for (int j = 0; j < c.getCategories().get(i)
+													.getItemList().size(); j++) {
+												if (c.getCategories().get(i).getItemList()
+														.get(j).getName().equals(itemName)) {
+													if(col == 0){
+														c.getCategories().get(i).getItemList().get(j).setName((String)table.getValueAt(row, col));
+													}else if(col == 1){
+														c.getCategories().get(i).getItemList().get(j).setEarnedPoints((String)table.getValueAt(row, col));
+													}else if(col == 2){
+														c.getCategories().get(i).getItemList().get(j).setTotalPoints((String)table.getValueAt(row, col));
+													}else if(col == 3){
+														try{
+															SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+															c.getCategories().get(i).getItemList().get(j).setUpdateDate(sdf.parse((String)table.getValueAt(row, col)));
+															c.getCategories().get(i).checkItemUpdateDate();
+															table.setValueAt(sdf.format(c.getCategories().get(i).getItemList().get(j).getUpdateDate()), row, col);
+															table.repaint();
+														}catch(Exception exp){
+															
+														}
+													}else if(col == 4){
+														c.getCategories().get(i).setName((String)table.getValueAt(row, col));
+													}
+													return;
+												}
+											}
+										}
+									}
+								}
+							}
+
+						}
+					}
+				}
+			}
+		}
+		
+	}
+	public void setQuarters(ArrayList<Quarter> quarters, String qt,
+			String course) {
+		this.quarters = quarters;
+		this.selectedCourse = course;
+		this.selectedQuarter = qt;
 	}
 
 	public JScrollPane getJScrollPane() {
 		return this.tableSP;
+	}
+
+	public void removeItem(int row, int column) {
+		for (Quarter current : quarters) {
+			if (current.getName().equals(selectedQuarter)) {
+				ArrayList<Course> currentCourses = current.getCourseList();
+
+				for (Course c : currentCourses) {
+					if (c.getCourseName().equals(selectedCourse)) {
+						String catName = (String) this.table.getValueAt(row, 4);
+						String itemName = (String) this.table
+								.getValueAt(row, 0);
+
+						for (int i = 0; i < c.getCategories().size(); i++) {
+							if (c.getCategories().get(i).getName()
+									.equals(catName)) {
+								for (int j = 0; j < c.getCategories().get(i)
+										.getItemList().size(); j++) {
+									if (c.getCategories().get(i).getItemList()
+											.get(j).getName().equals(itemName)) {
+										c.getCategories().get(i)
+												.removeItem(itemName);
+										this.tableModel.removeRow(row);
+										this.table.setModel(tableModel);
+										this.table.repaint();
+										return;
+									}
+								}
+							}
+						}
+					}
+				}
+
+			}
+		}
 	}
 
 	public void changeHeaderValues(String[] headings) {
@@ -70,7 +203,7 @@ public class SchoolBudGUITable {
 		for (int i = 0; i < numItems; i++) {
 			this.tableModel.addRow(names[i]);
 		}
-		
+
 		this.table.setModel(this.tableModel);
 	}
 
@@ -78,6 +211,10 @@ public class SchoolBudGUITable {
 		Object[][] data = {};
 		this.tableModel = new DefaultTableModel(data, this.columnNames);
 		this.table.setModel(this.tableModel);
+	}
+
+	public JTable getTable() {
+		return this.table;
 	}
 
 	class ButtonRenderer extends JButton implements TableCellRenderer {
